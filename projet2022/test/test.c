2dc7b075164f9665947ac6a83c2604a51140a459
ValentinMascaro <42706710+ValentinMascaro@users.noreply.github.com>
2022-03-20 16:45:38 +0100

    Add files via upload

--- projet2022/test/test.c
+++ projet2022/test/test.c
@@ -1,8 +1,41 @@
-int main()
-{
 int a;
-a = ahoiheioe;
+int main(){
+int b;
+
+    {
+        int c;
+        {
+            int d;
+            a = b + c * d;
+        }
+    }
+    {
+        int e;
+        
+    }
+    if(1==1)
+    {
+       
+    }
+    else{
+        
+    }
 return 0;
 }
 
+int f2(int F2A, int F2B)
+{
+  int F2C;
 
+}
+
+int f3()
+{
+ 
+}
+
+int f4()
+{
+   
+    return 1;
+}
